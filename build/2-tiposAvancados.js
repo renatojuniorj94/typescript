f0db950c370813596dcf646730f8f7bef15b0e2d
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2025-08-16 16:56:53 -0300

    Union type TS

---
+++ build/2-tiposAvancados.js
@@ -0,0 +1,7 @@
+"use strict";
+/* Tipos avançados ou especiais*/
+//Union type - Quando um valor pode ser mais do que um único tipo
+let myVariable; //pode ser uma string ou um número
+myVariable = "Hello";
+myVariable = 42;
+//myVariable = true > erro
---
+++ programador-completo/2-tiposAvancados.ts
@@ -0,0 +1,7 @@
+/* Tipos avançados ou especiais*/
+
+//Union type - Quando um valor pode ser mais do que um único tipo
+let myVariable: string | number; //pode ser uma string ou um número
+myVariable = "Hello";
+myVariable = 42;
+//myVariable = true > erro
