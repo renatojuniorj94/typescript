11335191662027d23e3f4e97cf2c08eb2b307b77
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2025-08-21 19:43:12 -0300

    Aliases TS

--- build/3-aliasesEInterfaces.js
+++ build/3-aliasesEInterfaces.js
@@ -5,4 +5,13 @@ Basicamente é criar os seus próprios tipos personalizados
 Os aliases de tipo permitem definir tipos com um nome personalizado (um alias) de tipos primitivos ou complexos.
 
 As interfaces são a mesma coisa que os aliases, exceto que só se aplicam ao tipo de objeto.
-*/ 
+*/
+//Exemplo de uso
+const uno = 2001;
+const gol = "Volkswagen";
+const TSI = "Up";
+const laFerrari = {
+    ano: 2002,
+    marca: 'Ferrari',
+    modelo: 'La Ferrari'
+};
--- programador-completo/3-aliasesEInterfaces.ts
+++ programador-completo/3-aliasesEInterfaces.ts
@@ -5,3 +5,23 @@ Os aliases de tipo permitem definir tipos com um nome personalizado (um alias) d
 
 As interfaces são a mesma coisa que os aliases, exceto que só se aplicam ao tipo de objeto.
 */
+
+//Aliases (Criando nossos tipos personalizados)
+type AnoVeiculo = number;
+type MarcaVeiculo = string;
+type ModeloVeiculo = string;
+type Carro = {
+    ano: AnoVeiculo,
+    marca: MarcaVeiculo,
+    modelo: ModeloVeiculo
+};
+
+//Exemplo de uso
+const uno: AnoVeiculo = 2001;
+const gol: MarcaVeiculo = "Volkswagen";
+const TSI: ModeloVeiculo = "Up";
+const laFerrari: Carro = {
+    ano: 2002,
+    marca: 'Ferrari',
+    modelo: 'La Ferrari'
+};
