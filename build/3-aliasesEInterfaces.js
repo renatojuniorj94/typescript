4e15400b09f43dbfda628da7b9fc86cf8f121f52
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2025-08-21 19:36:33 -0300

    Explanation

--- build/3-aliasesEInterfaces.js
+++ build/3-aliasesEInterfaces.js
@@ -1,1 +1,8 @@
 "use strict";
+/* Aliases e interfaces
+Basicamente é criar os seus próprios tipos personalizados
+
+Os aliases de tipo permitem definir tipos com um nome personalizado (um alias) de tipos primitivos ou complexos.
+
+As interfaces são a mesma coisa que os aliases, exceto que só se aplicam ao tipo de objeto.
+*/ 
