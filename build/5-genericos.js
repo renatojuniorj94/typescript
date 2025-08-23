21c1090f9528f686abb5981b2c2e30d91aa30c37
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2025-08-23 18:21:18 -0300

    Creating file "5-genericos"

---
+++ build/5-genericos.js
@@ -0,0 +1,4 @@
+"use strict";
+/* Generics (Genéricos)
+Pense nso generics como "caixas" flexíveis, onde você pode colocar diferentes tipos de dados. É um coringa, que você pode aceitar um valor.
+ */ 
---
+++ programador-completo/5-genericos.ts
@@ -0,0 +1,3 @@
+/* Generics (Genéricos)
+Pense nso generics como "caixas" flexíveis, onde você pode colocar diferentes tipos de dados. É um coringa, que você pode aceitar um valor.
+ */
