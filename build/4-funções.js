394135510bbaee41fafaff0beda840cbcc71ecfe
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2025-08-22 21:22:24 -0300

    Creating file "4-funções"

---
+++ build/4-funções.js
@@ -0,0 +1,5 @@
+"use strict";
+/* Funções
+
+TypeScript tem uma sintaxe específica para digitar parâmetros de função e valores de retorno.
+*/
---
+++ programador-completo/4-funções.ts
@@ -0,0 +1,4 @@
+/* Funções
+
+TypeScript tem uma sintaxe específica para digitar parâmetros de função e valores de retorno.
+*/
