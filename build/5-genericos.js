3291733f96c87c6240cb1c2d528433465b616e01
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2025-08-23 18:53:48 -0300

    function "imprimirValor()" with generics

--- build/5-genericos.js
+++ build/5-genericos.js
@@ -1,4 +1,9 @@
 "use strict";
 /* Generics (Genéricos)
 Pense nso generics como "caixas" flexíveis, onde você pode colocar diferentes tipos de dados. É um coringa, que você pode aceitar um valor.
- */ 
+ */
+function imprimirValor(valor) {
+    console.log(valor);
+}
+imprimirValor(42); // imprime 42
+imprimirValor('Olá'); // imprime 'Olá
--- programador-completo/5-genericos.ts
+++ programador-completo/5-genericos.ts
@@ -1,3 +1,10 @@
 /* Generics (Genéricos)
 Pense nso generics como "caixas" flexíveis, onde você pode colocar diferentes tipos de dados. É um coringa, que você pode aceitar um valor.
  */
+
+function imprimirValor<T>(valor: T): void {
+    console.log(valor);    
+}
+
+imprimirValor<number>(42); // imprime 42
+imprimirValor<string>('Olá'); // imprime 'Olá
