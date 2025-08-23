928d7c4ebb602dd026a74bf4dc39a75c4e4d732d
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2025-08-22 21:23:35 -0300

    Defining the return type

--- build/4-funções.js
+++ build/4-funções.js
@@ -3,3 +3,7 @@
 
 TypeScript tem uma sintaxe específica para digitar parâmetros de função e valores de retorno.
 */
+//Definindo o tipo do retorno da função
+function getTime() {
+    return new Date().getTime();
+}
--- programador-completo/4-funções.ts
+++ programador-completo/4-funções.ts
@@ -2,3 +2,8 @@
 
 TypeScript tem uma sintaxe específica para digitar parâmetros de função e valores de retorno.
 */
+
+//Definindo o tipo do retorno da função
+function getTime(): number {
+    return new Date().getTime();
+}
